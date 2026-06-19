d53e3fe9d17928b74bde9e202f6e44dc0edbbb76
jason <iasonas.tan@gmail.com>
2026-06-19 04:19:46 +0300

    Initial Commit

---
+++ app/src/main/java/com/morse/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.morse.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
